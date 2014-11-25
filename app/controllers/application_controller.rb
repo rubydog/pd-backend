98b5f435e9f10159c4236e0696c42a20cf732513
Bhushan Lodha <bhushanlodha@gmail.com>
2014-11-25 08:33:31 +0530

    Silenced activerecord logging for test env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < Sinatra::Base
   helpers ApplicationHelper
   enable :logging
 
+  configure :test do
+    ActiveRecord::Base.logger.level = Logger::INFO
+  end
+
   before do
     content_type :json
   end
