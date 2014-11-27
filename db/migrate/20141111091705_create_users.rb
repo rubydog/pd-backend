435ec3bda55fd9abfc2a4ca0a2157b995abe423d
Bhushan Lodha <bhushanlodha@gmail.com>
2014-11-27 11:22:16 +0530

    changed user attribute from mobile_number to mobile

--- db/migrate/20141111091705_create_users.rb
+++ db/migrate/20141111091705_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :mobile_number
+      t.string :mobile
       t.integer :role, default: 0
       t.references :college
       t.references :department
