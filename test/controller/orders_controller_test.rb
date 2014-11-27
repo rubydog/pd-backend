ca7a130c4d78097871b121676223205e5840f94f
Bhushan Lodha <bhushanlodha@gmail.com>
2014-11-27 15:53:16 +0530

    working on order index action

--- test/controller/orders_controller_test.rb
+++ test/controller/orders_controller_test.rb
@@ -18,4 +18,22 @@ class OrdersControllerTest < MiniTest::Test
     assert last_response.ok?
     assert order_count + 1, Order.count
   end
+
+  # test index
+  # def test_index
+  #   user1 = create :user, mobile: '9988776655'
+  #   user2 = create :user, mobile: '9988776644'
+  #   user3 = create :user, mobile: '9988776633'
+  #   listing1 = create :listing, user: user1
+  #   listing2 = create :listing, user: user2
+  #   listing3 = create :listing, user: user3
+  #   order1 = create :order, listing: listing1, buyer: user2
+  #   order2 = create :order, listing: listing2, buyer: user1
+
+  #   get '/', mobile: user1.mobile
+
+  #   assert last_response.ok?
+  #   assert_match order1.serialized_hash.to_json, last_response.body
+  # end
+
 end
