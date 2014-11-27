afb8f48fcfa0a4400ea09cca795135b87032149e
Bhushan Lodha <bhushanlodha@gmail.com>
2014-11-27 12:37:40 +0530

    a mistaken comming?

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,21 @@
 class OrdersController < ApplicationController
-  # before '/create' do
-  #   if params[:order].present?
-  #     params[:order][:listing_id] = params[:order][:listing_id].to_i \
-  #       if params[:order][:listing_id].present?
-  #     params [:order][:
-  #   end
-  # end
+  before '/create' do
+    if params[:order].present?
+      params[:order][:listing_id] = params[:order][:listing_id].to_i \
+                                         if params[:order][:listing_id].present?
+      params[:order][:seller_id] = params[:order][:seller_id].to_i \
+                                         if params[:order][:seller_id].present?
+      params[:order][:buyer_id] = params[:order][:buyer_id].to_i \
+                                         if params[:order][:buyer_id].present?
+    end
+  end
+
+  post '/create' do
+    order = Order.new(params[:order])
+    if order.save
+      # success
+    else
+      # errou
+    end
+  end
 end
