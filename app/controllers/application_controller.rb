f83586fc476807113d7049552f5619b218c12988
Bhushan Lodha <bhushanlodha@gmail.com>
2014-12-12 13:24:54 +0530

    Whitelisted localhost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
   enable :logging
   enable :raise_errors
   set :protection, origin_whitelist: ["http://www.pajamadeals.in",
-                                      "http://pajamadeals.in"]
+                                      "http://pajamadeals.in",
+                                      "http://localhost:9393"]
 
   configure :test do
     ActiveRecord::Base.logger = nil
@@ -18,15 +19,15 @@ class ApplicationController < Sinatra::Base
 
   before do
     # request.url =~ /^https?:\/\/([\da-z\.-]+)?pajamadeals\.in/)
-    unless ENV['RACK_ENV'] == 'test' || ENV['RACK_ENV'] == 'development' || \
-                          (ENV['RACK_ENV'] == 'production' && 
-                           request.url =~ /^https?:\/\/(wwww.)?pajamadeals\.in/)
-      halt 403, "403 Forbidden Entry.\n “I’m sorry. I know who you are–I believe
-                 who you say you are–but you just don’t have permission to 
-                 access this resource. Maybe if you ask the system administrator
-                 nicely, you’ll get permission. But please don’t bother me again
-                 until your predicament changes."
-    end
+    # unless ENV['RACK_ENV'] == 'test' || ENV['RACK_ENV'] == 'development' || \
+    #                       (ENV['RACK_ENV'] == 'production' &&
+    #                        request.url =~ /^https?:\/\/(wwww.)?pajamadeals\.in/)
+    #   halt 403, "403 Forbidden Entry.\n “I’m sorry. I know who you are–I believe
+    #              who you say you are–but you just don’t have permission to
+    #              access this resource. Maybe if you ask the system administrator
+    #              nicely, you’ll get permission. But please don’t bother me again
+    #              until your predicament changes."
+    # end
     content_type :json
   end
 
