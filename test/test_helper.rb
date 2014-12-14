70997ba116c025d6fa5eb098b4fe8f8703359cf6
Bhushan Lodha <bhushanlodha@gmail.com>
2014-12-14 23:40:03 +0530

    Added json format for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,4 +28,41 @@ class MiniTest::Test
     DatabaseCleaner.strategy = :truncation, { pre_count: true }
     DatabaseCleaner.clean
   end
+  
+  def last_json_response
+    ::JSON.parse(last_response.body)
+  end
+
+  def get_json(path, params = {}, headers = {})
+    json_request :get, path, params, headers
+  end
+
+  def post_json(path, params = {}, headers = {})
+    json_request :post, path, params, headers
+  end
+
+  def put_json(path, params = {}, headers = {})
+    json_request :put, path, params, headers
+  end
+
+  def delete_json(path, params = {}, headers = {})
+    json_request :delete, path, params, headers
+  end
+
+  private
+
+  def json_request(verb, path, params = {}, headers = {})
+    send verb, path, params.to_json, headers.merge({ "CONTENT_TYPE" => "application/json" })
+  end
+end
+
+
+require "json"
+
+module Rack
+  module Minitest
+    module JSON
+      
+    end
+  end
 end
